9a53039388fb58279ac32d46e3e427ea54ad3f35
DanilodeMelo-coder <danilo.demelo.0207@gmail.com>
2026-04-24 19:24:27 -0300

    refactor(structure): separar models e schemas na arquitetura

---
+++ schemas/aluno.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import date
+import uuid
+
+class Aluno(BaseModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4)
+    nome: str
+    email: str
+    data_nascimento: date
+    cpf: str
+    admin: bool = False
+
+class Criar_aluno(BaseModel):
+    nome: str
+    email: str
+    data_nascimento: date
+    cpf: str
+    admin: Optional [bool]
+
+class AlunoUpdate(BaseModel):
+    nome: str
+    email: str
+    data_nascimento: date
+    admin: Optional [bool]
