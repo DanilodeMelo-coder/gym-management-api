714b4c4008b2611e698c22731b13c1b49d27acd8
DanilodeMelo-coder <danilo.demelo.0207@gmail.com>
2026-04-24 20:36:44 -0300

    feat: adicionar criação automática das tabelas no main  da aplicação

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routes.aluno_routes import router as aluno_router
 from routes.exercicio_routes import router as exercicio_router
+from core.database import engine, Base
 
+Base.metadata.create_all(bind= engine)
 
 app = FastAPI()
 
