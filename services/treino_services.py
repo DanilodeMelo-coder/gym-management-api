1a51696b38f3af5f950ff6d685091f5d344fb4eb
unknown <danilo.demelo.0207@gmail.com>
2026-06-25 12:21:49 -0300

    feat: implementar services de treino (criar, listar, buscar, deletar, adicionar/remover exercicio)

--- services/treino_services.py
+++ services/treino_services.py
@@ -2,6 +2,9 @@ from sqlalchemy.orm import Session
 from schemas.treino import CriarTreino
 import uuid
 from models.treino import Treino
+from schemas.treino import AdicionarExercicio
+from models.exercicio import Exercicio
+from models.treinoExercicio import TreinoExercicio
 
 
 def criar_treino(payload: dict, criar_treino: CriarTreino, db: Session):
@@ -58,5 +61,62 @@ def deletar_treino(id: str, db: Session):
 
     return {"status": "sucesso", 
     "mensagem": "Treino deletado com sucesso", 
+    "data": None
+    }
+
+
+def adicionar_exercicio_service(treino_id: str, adicionar_exercicio: AdicionarExercicio, db: Session ):
+    treino = db.query(Treino).filter(Treino.id == treino_id).first()
+
+    if not treino:
+        return {"status": "erro", 
+        "mensagem": "Treino não encontrado", 
+        "data": None
+        }
+    
+    exercicio_id = adicionar_exercicio.exercicio_id
+
+    exercicio = db.query(Exercicio).filter(Exercicio.id == exercicio_id).first()
+
+    if not exercicio:
+        return {"status": "erro", 
+        "mensagem": "Exercicio não encontrado", 
+        "data": None
+        }
+
+
+    treino_exercicio_novo = TreinoExercicio(
+        id= str(uuid.uuid4()),
+        treino_id= treino.id,
+        exercicio_id = exercicio_id,
+        series = adicionar_exercicio.series,
+        repeticoes= adicionar_exercicio.repeticoes,
+        carga= adicionar_exercicio.carga
+    )
+
+    db.add(treino_exercicio_novo)
+    db.commit()
+    db.refresh(treino_exercicio_novo)
+
+    return {"status": "sucesso", 
+        "mensagem": "Exercicio adicionado com sucesso", 
+        "data": treino_exercicio_novo
+        }
+
+
+def deletar_exercicio(id: str, db: Session):
+    treino_exercicio =db.query(TreinoExercicio).filter(TreinoExercicio.id == id).first()
+
+    if not treino_exercicio:
+        return {"status": "erro", 
+        "mensagem": "Treino não encontrado", 
+        "data": None
+        }
+    
+    db.delete(treino_exercicio)
+    db.commit()
+
+    return {"status": "sucesso", 
+    "mensagem": "Exercicio deletado com sucesso", 
     "data": None
     }
