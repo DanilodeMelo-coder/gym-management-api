97d1e85971e1a74fb6364758cea7693985e61ccd
unknown <danilo.demelo.0207@gmail.com>
2026-06-26 14:07:02 -0300

    feat: adicionar rotas de treino

---
+++ routes/treinoexercicio.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, HTTPException
+from services.treino_services import listar_treinos, criar_treino, buscar_treino, deletar_treino, adicionar_exercicio_service, deletar_exercicio
+from schemas.treino import CriarTreino, AdicionarExercicio
+from core.database import get_session
+from sqlalchemy.orm import Session
+from dependencias.auth_depends import autenticar_token, apenas_admin, apenas_personal
+
+
+router = APIRouter(prefix="/Treinos", tags=["Treinos"])
+
+@router.get("/")
+def listar_treinos(db: Session = Depends(get_session), payload= Depends(autenticar_token)):
+    return listar_treinos(payload, db)
+
+@router.post("/")
+def criar_treino(criartreino: CriarTreino, db: Session = Depends(get_session), payload= Depends(autenticar_token)):
+    return criar_treino(payload, criartreino, db)
+
+@router.get("/{id}")
+def buscar_treino(id: str, db: Session = Depends(get_session), payload = Depends(autenticar_token)):
+    return buscar_treino(id, db)
+
+@router.delete("/{id}")
+def deletar_treino(id: str, db: Session = Depends(get_session), payload = Depends(autenticar_token)):
+    return deletar_treino(id, db)
+
+@router.post("/{id}/exercicios")
+def adicionar_exercicio_treino(id: str, exercicio: AdicionarExercicio, db: Session = Depends(get_session), payload = Depends(autenticar_token)):
+    return adicionar_exercicio_service(id, exercicio, db)
+
+@router.delete("/exercicios/{id}")
+def deletar_exercicio_treino(id: str, db: Session = Depends(get_session), payload = Depends(autenticar_token)):
+    return deletar_exercicio(id, db)
+
